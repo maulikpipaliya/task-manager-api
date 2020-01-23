896dc0e3a5a34cc62e6d45eec4adbc67ad9ba9d4
maulikpipaliya <maulik.pipaliya@gmail.com>
2020-01-23 12:29:07 +0530

    S11-86 Added Data Validation and Sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
-
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const connectionURL = 'mongodb://127.0.0.1:27017/';
 const databaseName = 'task-manager-api';
@@ -13,23 +13,65 @@ mongoose.connect(dbURL, {
 
 /**
  * Created model User
- * 
- 
-const User = mongoose.model('User',{
-    name : {
-        type: String
+ *
+ */
+
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
-    age : {
-        type: Number
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email invalid');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength:7,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Value can\'t be password');
+            }
+
+        }
+
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
+/**
+ * add User
+ * 
+ * 
+ 
+
 const me = new User({
-    name: 'Joyy',
+    name: '  Maulik',
+    email: ' maUlik.pipaliyA@gmail.com',
+    password: '12345',
     age: 23
 });
 
-me.save().then( () => {
+me.save().then(() => {
     console.log(me);
 }).catch((error) => {
     console.log(error);
@@ -40,15 +82,25 @@ me.save().then( () => {
 /**
  * Creating Task model
  */
+ 
 
 const Task = mongoose.model('Task', {
-    description: String,
-    completed: Boolean
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    } 
 })
 
+/**
+ * add Task
+ */
 const task1 = new Task({
-    description: 'Task 1 ',
-    completed: true
+    description: ' task 2 '
 })
 
 
