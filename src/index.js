6b3b9ed43a378197dba5951b0700e159eb13dc22
maulikpipaliya <maulik.pipaliya@gmail.com>
2020-01-23 14:26:30 +0530

    S11-90 Added /tasks REST API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose')
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,15 +11,27 @@ app.use(express.json());
 app.post('/users', (request, response) => {
     console.log(request.body);
     const user = new User(request.body);
-    user.save().then( (result) => {
+    user.save().then((result) => {
         console.log(result);
-        response.send(user);
-    }).catch( (error) => {
+        response.status(201).send(user);
+    }).catch((error) => {
         response.status(400).send(error);
     })
     // response.send('Testing');
+})
 
+app.post('/tasks', (request, response) => {
+    const requestBody = request.body;
+    console.log(requestBody);
 
+    const task = new Task(requestBody);
+
+    task.save().then((result) => {
+        response.status(201).send(task);
+        console.log(result);
+    }).catch((error) => {
+        response.status(400).send(error);
+    })
 })
 
 app.listen(port, () => {
