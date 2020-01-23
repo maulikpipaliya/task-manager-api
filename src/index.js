bdae4ac5b0d9285ed97c02b7bf7c2f814457edb8
maulikpipaliya <maulik.pipaliya@gmail.com>
2020-01-23 14:58:52 +0530

    S11-91 Added READ/ADD /tasks and /users API

--- src/index.js
+++ src/index.js
@@ -8,17 +8,36 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (request, response) => {
-    console.log(request.body);
-    const user = new User(request.body);
-    user.save().then((result) => {
-        console.log(result);
-        response.status(201).send(user);
-    }).catch((error) => {
-        response.status(400).send(error);
+app.get('/tasks', (request,response) => {
+
+    Task.find({}).then( (tasks) => {
+        if(tasks){
+            response.status(200).send(tasks);
+        }
+    } ).catch( (error) => {
+        response.status(500).send(error);
     })
-    // response.send('Testing');
+
+    // response.status(200).send(request.body);
 })
+
+app.get('/tasks/:id', (request,response) => {
+    const _id = request.params.id;
+
+    Task.findById(_id).then( (task) => {
+        if(task){
+            response.status(200).send(task);
+        }else{
+            response.status(404).send();
+        }
+    } ).catch( (error) => {
+        response.status(500).send(error);
+    })
+
+    // response.status(200).send(request.body);
+})
+
+
 
 app.post('/tasks', (request, response) => {
     const requestBody = request.body;
@@ -33,6 +52,45 @@ app.post('/tasks', (request, response) => {
         response.status(400).send(error);
     })
 })
+
+
+app.get('/users', (request, response) => {
+    User.find({}).then((users) => {
+        response.status(200).send(users);
+    }).catch((error) => {
+        response.status(500).send();
+    })
+})
+
+
+app.get('/users/:id', (request, response) => {
+
+    const _id = request.params.id;
+
+    User.findById(_id).then((result) => {
+        if (!result) {
+            response.status(404).send();
+        }
+
+        response.send(result);
+    }).catch((error) => {
+        response.status(500).send(error);
+    })
+
+})
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+    const user = new User(request.body);
+    user.save().then((result) => {
+        console.log(result);
+        response.status(201).send(user);
+    }).catch((error) => {
+        response.status(400).send(error);
+    })
+    // response.send('Testing');
+})
+
 
 app.listen(port, () => {
     console.log('Listening on ', port);
